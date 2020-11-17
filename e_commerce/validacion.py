e113accbbfc0e0446e18977e2d38401171a5d182
sofibarbona <73912495+sofibarbona@users.noreply.github.com>
2020-11-16 21:13:00 -0300

    Update validacion.py

--- e_commerce/validacion.py
+++ e_commerce/validacion.py
@@ -9,7 +9,6 @@ class validator():
     def validar_usuario(self,dic):
         datosFinales={}
         errores={}
-        SpecialSym=['$','@','#','%']
         for x,y in dic.items():
             datosFinales[x]=y.strip()
 
@@ -19,31 +18,23 @@ class validator():
             errores['fechanac']='campo fecha de nacimiento vacio'
         if datosFinales['sexo']=='':
             errores['sexo']='campo sexo vacio'
+        if datosFinales['telefono']=='':
+            errores['telefono']='campo telefono vacio'
         if datosFinales["email"]=="":
             errores["email"] = "campo email vacio"
         elif validate_email(datosFinales["email"])==False:
             errores["email"] = "El email no es un email"
         if datosFinales['ciudad']=='':
             errores['ciudad']='campo ciudad vacio'
-        if datosFinales['telefono']=='':
-            errores['telefono']='campo telefono vacio'
 
         
         if len(datosFinales["password"])< 6:
             errores["password"]='password debe tener mas de 6 caracteres'
         elif datosFinales["password"]=="":
             errores["password"]='campo password vacio'
-        elif not any(char.isdigit() for char in datosFinales["password"]):
-            errores["password"]='Password debe tener un caracter numeral'
-        elif not any(char.isupper() for char in datosFinales["password"]):
-            errores["password"]='Password debe tener una palabra mayuscula'
-        elif not any(char.islower() for char in datosFinales["password"]):
-            errores["password"]='Password debe tener minusculas'
-        elif not any(char in SpecialSym for char in datosFinales["password"]):
-            errores["password"]='Password debe tener al menos un caracter especial $@#'
         
         if datosFinales["cpassword"]=="":
-            errores["cpassword"] = "Hubo error en la confirmacion de contrasenia porque esta vacia"
+            errores["cpassword"] = "Hubo error en la confirmacion de password porque esta vacia"
         elif datosFinales["cpassword"] != datosFinales["password"]:
             errores["cpassword"] = "Password no verifica"
 
@@ -55,6 +46,7 @@ class validator():
             if result is not None:
                 errores['mail']='el mail ya esta registrado en nuestra base'
                 return errores
-        
+
         return errores
 
+validator=validator()
