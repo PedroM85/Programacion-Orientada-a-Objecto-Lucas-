a8521f19094604ff2d4893688fbdd19fdd3c3eb3
sofibarbona <73912495+sofibarbona@users.noreply.github.com>
2020-11-16 21:10:22 -0300

    Update prueba.py

--- e_commerce/prueba.py
+++ e_commerce/prueba.py
@@ -5,22 +5,23 @@ import getpass
 
 formusuario={}
 formusuario['nombrecom']=input('Nombre completo: ')
-formusuario['fechanac']=input('Fecha de Nacimiento: ')
-formusuario['sexo']=input('Sexo: ')
+formusuario['fechanac']=input('Fecha de Nacimiento AAAA-MM-DD: ')
+formusuario['sexo']=input('Sexo F o M: ')
+formusuario['telefono']=input('Telefono: ')
 formusuario['email']=input('Email: ')
 formusuario['ciudad']=input('Ciudad: ')
 formusuario['password']=getpass.getpass('Password: ')
 formusuario['cpassword']=getpass.getpass('Confirmar Password: ')
 
-print(validator.validar_usuario(formusuario))
-"""if validator.validar_usuario(formusuario)=={}:
-    user=Usuario(formusuario['nombrecom'],formusuario['fechanac'],formusuario['sexo'],formusuario['email'],formusuario['ciudad'],formusuario['password'],formusuario['cpassword'])
+
+if validator.validar_usuario(formusuario)=={}:
+    user=Usuario(formusuario['nombrecom'],formusuario['fechanac'],formusuario['sexo'],formusuario['telefono'],formusuario['email'],formusuario['ciudad'],formusuario['password'])
     user.save()
     print('usuario registrado exitosamente')
 else:
-    print(validator.validar_usuario(formusuario))"""
+    print(validator.validar_usuario(formusuario))
 
 #user = Usuario("Pedro Maneiro","1985-09-10","M",1121651051,"pedromaneiro@gmail.com", 1 , "pedro1985")
 #print(user.get_ciudad)
-#user.save()
+#user.save()s
 #print(user)
